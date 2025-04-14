4021d0ed4db152f8e5efc9058c1b1b8612f6e6c3
조수민 <sumin0310@duksung.ac.kr>
2025-04-15 00:36:58 +0900

    🎨Design: Login_add

--- app/src/main/java/com/example/nogorok/SignupLoginFragment.kt
+++ app/src/main/java/com/example/nogorok/SignupLoginFragment.kt
@@ -1,25 +1,19 @@
 package com.example.nogorok
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class SignupLoginFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_signup_login, container, false)
 
-        view.findViewById<Button>(R.id.btn_signup).setOnClickListener {
-            startActivity(Intent(requireContext(), RegisterActivity::class.java))
-        }
-
-        view.findViewById<Button>(R.id.btn_login).setOnClickListener {
-            startActivity(Intent(requireContext(), LoginActivity::class.java))
-        }
-
-        return view
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Fragment의 레이아웃 파일 연결
+        return inflater.inflate(R.layout.fragment_signup_login, container, false)
     }
 }
