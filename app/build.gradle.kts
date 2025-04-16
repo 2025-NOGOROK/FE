c1f7c55ac57fc77a7e470755e59acde7f5691561
조수민 <sumin0310@duksung.ac.kr>
2025-04-17 01:33:03 +0900

    ⚙️ chore: gradle 및 라이브러리 설정 main 브랜치와 동기화
    - build.gradle.kts, settings.gradle 등 main 브랜치 기준으로 업데이트
    - 라이브러리 버전/플러그인 설정 최신화

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.nogorok"
-        minSdk = 26
+        minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -76,4 +76,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
 }
