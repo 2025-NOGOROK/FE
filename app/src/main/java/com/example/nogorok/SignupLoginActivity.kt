e9e9167407da219014b453e9e41fe60ff6979bf2
조수민 <sumin0310@duksung.ac.kr>
2025-05-08 19:40:38 +0900

    ✨ 회원가입 플로우 및 전체 액티비티 매니페스트 통합, 회원가입 버튼 클릭 시 약관동의(RegisterTermsActivity)로 이동하도록 수정, 🎨 splash_bg 색상 리소스 추가 및 빌드 오류 수정 ----- 스플래쉬 색상, 약관 동의 체크버튼 색상, 설문조사 박스 색상 수정 필요(안드로이드 기본 색상 오류)

--- app/src/main/java/com/example/nogorok/SignupLoginActivity.kt
+++ app/src/main/java/com/example/nogorok/SignupLoginActivity.kt
@@ -10,11 +10,17 @@ class SignupLoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_login)
 
+        // 회원가입 버튼 클릭 시 약관동의 페이지로 이동!
         findViewById<MaterialButton>(R.id.btnSignup).setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
+            // RegisterTermsActivity로 이동
+            val intent = Intent(this, RegisterTermsActivity::class.java)
+            startActivity(intent)
         }
+
+        // 로그인 버튼 클릭 시 로그인 페이지로 이동
         findViewById<MaterialButton>(R.id.btnLogin).setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
