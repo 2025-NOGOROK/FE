6708e59c868590a0648ec44e0311d80354258102
jiwonee02 <nepthun@naver.com>
2025-05-20 17:54:22 +0900

    ✨Feat: 플로팅버튼 구현

---
+++ app/src/main/java/com/example/nogorok/features/rest/longrest/LongRestActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nogorok.features.rest.longrest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nogorok.R
+
+class LongRestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_long_rest)
+    }
+}
---
+++ app/src/main/java/com/example/nogorok/features/rest/shortrest/ShortRestActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nogorok.features.rest.shortrest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nogorok.R
+
+class ShortRestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_short_rest)
+    }
+}
