fd82643f4766addc2ca329e9437d65fd31dd1190
조수민 <sumin0310@duksung.ac.kr>
2025-05-24 00:44:42 +0900

    🎨 Design: 나의 일정페이지 중앙 일정 UI 완료

--- app/src/main/java/com/example/nogorok/features/schedule/Schedule.kt
+++ app/src/main/java/com/example/nogorok/features/schedule/Schedule.kt
@@ -8,5 +8,6 @@ data class Schedule(
     val startDate: Date,       // 일정 시작 시간
     val endDate: Date,         // 일정 종료 시간
     val alarmOption: String,   // 알림 옵션
-    val moveAlarm: Boolean     // 이동 알림 여부
+    val moveAlarm: Boolean,     // 이동 알림 여부
+    val type: String = "fixed"
 )
